cca89dd202aa3d824cd8dc2e4f2ee2c965a52232
Adam Misiak <adammisiak0@gmail.com>
2021-12-20 10:29:20 +0100

    Implemented diagonal win veryfication and tests

--- board.py
+++ board.py
@@ -24,34 +24,63 @@ class Board:
             raise OutOfRangeError("Tile is out of range")
 
     def check_order_win(self, symbol):
-        searched = [symbol, symbol, symbol, symbol, symbol]
         board = self._create_board_in_list_form()
-        if self._check_horizontal(board, searched):
+        if self._check_horizontal(board, symbol):
             return True
-        if self._check_vertical(board, searched):
+        if self._check_vertical(board, symbol):
             return True
-
-    def _check_horizontal(self, board, searched):
-        for row in board:
-            if row[:size-1] == searched:
-                return True
-            if row[1:size] == searched:
-                return True
+        if self._check_diagonal(board, symbol):
+            return True
         return False
 
-    def _check_vertical(self, board, searched):
+    def _check_horizontal(self, board, symbol):
+        searched = [symbol, symbol, symbol, symbol, symbol]
+        avoided = [symbol, symbol, symbol, symbol, symbol, symbol]
+        for row in board:
+            if row[:size-1] == searched:
+                if row != avoided:
+                    return True
+            if row[1:size] == searched:
+                if row != avoided:
+                    return True
+        return False
+
+    def _check_vertical(self, board, symbol):
+        searched = [symbol, symbol, symbol, symbol, symbol]
+        avoided = [symbol, symbol, symbol, symbol, symbol, symbol]
         for i in range(size):
             tem_list = []
             for row in board:
                 tem_list.append(row[i])
             if tem_list[:size-1] == searched:
-                return True
+                if tem_list != avoided:
+                    return True
             if tem_list[1:size] == searched:
-                return True
+                if tem_list != avoided:
+                    return True
         return False
 
-    def _check_diagonal(self, board, searched):
-        pass
+    def _check_diagonal(self, b, symbol):
+        searched = [symbol, symbol, symbol, symbol, symbol]
+        avoided = [symbol, symbol, symbol, symbol, symbol, symbol]
+        diagonal_l_1 = [b[0][0], b[1][1], b[2][2], b[3][3], b[4][4], b[5][5]]
+        diagonal_l_2 = [b[0][5], b[1][4], b[2][3], b[3][2], b[4][1], b[5][0]]
+        diagonal_s_3 = [b[1][0], b[2][1], b[3][2], b[4][3], b[5][4]]
+        diagonal_s_4 = [b[4][0], b[3][1], b[2][2], b[1][3], b[0][4]]
+        diagonal_s_5 = [b[5][1], b[4][2], b[3][3], b[2][4], b[5][1]]
+        diagonal_s_6 = [b[0][1], b[1][2], b[2][3], b[3][4], b[4][5]]
+        diagonal_long = [diagonal_l_1, diagonal_l_2]
+        diagonal_short = [diagonal_s_3, diagonal_s_4, diagonal_s_5, diagonal_s_6]
+        if searched in diagonal_short:
+            return True
+        for diagonal in diagonal_long:
+            if diagonal[:size-1] == searched:
+                if diagonal != avoided:
+                    return True
+            if diagonal[1:size] == searched:
+                if diagonal != avoided:
+                    return True
+
 
     def _create_board_in_list_form(self):
         final_list = []
@@ -109,9 +138,10 @@ if __name__ == "__main__":
     bo.write_tile("a2", "O")
     bo.write_tile("a1", "O")
     bo.write_tile("a4", "O")
+    bo.write_tile("f4", "O")
     bo.write_tile("a5", "O")
     bo.write_tile("f1", "O")
     bo.write_tile("d5", "X")
+    print(bo._create_board_in_list_form())
     bo.generate_board()
-    print(bo.create_board_in_list_form())
     print(bo.get_board())
--- test_board.py
+++ test_board.py
@@ -33,6 +33,7 @@ def test_win_x():
     bo.write_tile("c1", "X")
     bo.write_tile("d1", "X")
     bo.write_tile("e1", "X")
+    bo.write_tile("f1", "O")
     assert bo.check_order_win("X")
 
 
@@ -54,4 +55,127 @@ def test_win_vertical():
     bo.write_tile("a4", "X")
     bo.write_tile("a5", "X")
     bo.write_tile("a6", "O")
+    assert bo.check_order_win("X")
+
+
+def test_not_win_with_6_in_vertical():
+    bo = Board()
+    bo.write_tile("b1", "X")
+    bo.write_tile("b2", "X")
+    bo.write_tile("b3", "X")
+    bo.write_tile("b4", "X")
+    bo.write_tile("b5", "X")
+    bo.write_tile("b6", "X")
+    assert not bo.check_order_win("X")
+
+
+def test_not_win_with_6_in_horizontal():
+    bo = Board()
+    bo.write_tile("a1", "X")
+    bo.write_tile("b1", "X")
+    bo.write_tile("c1", "X")
+    bo.write_tile("d1", "X")
+    bo.write_tile("e1", "X")
+    bo.write_tile("f1", "X")
+    assert not bo.check_order_win("X")
+
+
+def test_diagonal_long_1_option():
+    bo = Board()
+    bo.write_tile("a1", "X")
+    bo.write_tile("b2", "X")
+    bo.write_tile("c3", "X")
+    bo.write_tile("d4", "X")
+    bo.write_tile("e5", "X")
+    assert bo.check_order_win("X")
+
+def test_diagonal_long_2_option():
+    bo = Board()
+    bo.write_tile("f6", "X")
+    bo.write_tile("b2", "X")
+    bo.write_tile("c3", "X")
+    bo.write_tile("d4", "X")
+    bo.write_tile("e5", "X")
+    assert bo.check_order_win("X")
+
+
+def test_diagonal_long_3_option():
+    bo = Board()
+    bo.write_tile("a6", "X")
+    bo.write_tile("b5", "X")
+    bo.write_tile("c4", "X")
+    bo.write_tile("d3", "X")
+    bo.write_tile("e2", "X")
+    assert bo.check_order_win("X")
+
+
+def test_diagonal_long_4_option():
+    bo = Board()
+    bo.write_tile("f1", "X")
+    bo.write_tile("b5", "X")
+    bo.write_tile("c4", "X")
+    bo.write_tile("d3", "X")
+    bo.write_tile("e2", "X")
+    assert bo.check_order_win("X")
+
+
+def test_diagonal_long_5_option_lose():
+    bo = Board()
+    bo.write_tile("f1", "X")
+    bo.write_tile("b5", "X")
+    bo.write_tile("c4", "X")
+    bo.write_tile("d3", "X")
+    bo.write_tile("e2", "X")
+    bo.write_tile("a6", "X")
+    assert not bo.check_order_win("X")
+
+
+def test_diagonal_long_6_option_lose():
+    bo = Board()
+    bo.write_tile("a1", "X")
+    bo.write_tile("b2", "X")
+    bo.write_tile("c3", "X")
+    bo.write_tile("d4", "X")
+    bo.write_tile("e5", "X")
+    bo.write_tile("f6", "X")
+    assert not bo.check_order_win("X")
+
+
+def test_diagonal_short_1():
+    bo = Board()
+    bo.write_tile("a5", "X")
+    bo.write_tile("b4", "X")
+    bo.write_tile("c3", "X")
+    bo.write_tile("d2", "X")
+    bo.write_tile("e1", "X")
+    assert bo.check_order_win("X")
+
+
+def test_diagonal_short_2():
+    bo = Board()
+    bo.write_tile("b6", "X")
+    bo.write_tile("c5", "X")
+    bo.write_tile("d4", "X")
+    bo.write_tile("e3", "X")
+    bo.write_tile("f2", "X")
+    assert bo.check_order_win("X")
+
+
+def test_diagonal_short_3():
+    bo = Board()
+    bo.write_tile("e6", "X")
+    bo.write_tile("d5", "X")
+    bo.write_tile("c4", "X")
+    bo.write_tile("b3", "X")
+    bo.write_tile("a2", "X")
+    assert bo.check_order_win("X")
+
+
+def test_diagonal_short_4():
+    bo = Board()
+    bo.write_tile("b1", "X")
+    bo.write_tile("c2", "X")
+    bo.write_tile("d3", "X")
+    bo.write_tile("e4", "X")
+    bo.write_tile("f5", "X")
     assert bo.check_order_win("X")
