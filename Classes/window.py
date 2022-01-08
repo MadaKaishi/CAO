4fd2270a0f1126290cfe556335f8171281f544c1
Adam Misiak <madakaishi@gmail.com>
2022-01-08 13:33:48 +0100

    Defined general window creator function

--- Classes/window.py
+++ Classes/window.py
@@ -1,7 +1,7 @@
 import pygame
 from pygame.constants import K_ESCAPE, MOUSEBUTTONDOWN
 from os.path import exists
-from .constants import BLACK, BUTTON_1_CORDS, BUTTON_2_CORDS, BUTTON_HEIGHT, BUTTON_WIDTH, FONT_TITLE_BUTONS, FONT_TITLE_HEADER, GREEN, GREY, PATH, SQUARE_SIZE, WHITE
+from .constants import BLACK, BUTTON_1_CORDS, BUTTON_2_CORDS, BUTTON_HEIGHT, BUTTON_WIDTH, FONT_TITLE_BUTONS, FONT_TITLE_HEADER, GREEN, GREY, HEADER_CORDS, PATH, SQUARE_SIZE, WHITE, WIDTH
 
 
 class Window:
@@ -57,13 +57,12 @@ class Window:
         textrect.topleft = (x, y)
         self._win.blit(textobj, textrect)
 
-    def draw_text_v2(self, text, font, color, x, y):
+    def draw_text_auto_centered(self, text, font, color, y):
         textobj = font.render(text, 1, color)
-        textrect = textobj.get_rect(center=(BUTTON_WIDTH//2, y + BUTTON_HEIGHT//2))
-        #textrect.center = (x + BUTTON_WIDTH/2, y + BUTTON_HEIGHT/2)
+        textrect = textobj.get_rect(center=(WIDTH//2, y + BUTTON_HEIGHT//2))
         self._win.blit(textobj, textrect)
 
-    def generate_basic_window(self, text_title, text_button_1, text_button_2):
+    def _generate_basic_window_other_than_title(self, text_title, text_button_1, text_button_2, action1, action2):
         run = True
         button_new_game = pygame.Rect(BUTTON_1_CORDS[0], BUTTON_1_CORDS[1], BUTTON_WIDTH, BUTTON_HEIGHT)
         button_load_game = pygame.Rect(BUTTON_2_CORDS[0], BUTTON_2_CORDS[1], BUTTON_WIDTH, BUTTON_HEIGHT)
@@ -73,23 +72,22 @@ class Window:
             self._win.fill(WHITE)
             font_title = pygame.font.SysFont(None, FONT_TITLE_HEADER)
             font_button = pygame.font.SysFont(None, FONT_TITLE_BUTONS)
-            self.draw_text_v2(f"{text_title}", font_title, BLACK, 100, 100)
+            self.draw_text_auto_centered(f"{text_title}", font_title, BLACK, HEADER_CORDS[1])
             pygame.draw.rect(self._win, GREY, button_new_game)
             pygame.draw.rect(self._win, GREY, button_load_game)
-            self.draw_text(f"{text_button_1}", font_button, BLACK, 225, 310)
-            self.draw_text(f"{text_button_2}", font_button, BLACK, 225, 435)
+            self.draw_text_auto_centered(f"{text_button_1}", font_button, BLACK, BUTTON_1_CORDS[1])
+            self.draw_text_auto_centered(f"{text_button_2}", font_button, BLACK, BUTTON_2_CORDS[1])
             if button_load_game.collidepoint((x, y)):
-                if self._is_save:
-                    pygame.draw.rect(self._win, GREEN, button_load_game)
-                    self.draw_text("Load game", font_button, BLACK, 225, 435)
-                    if click:
-                        self._action = "Load"
-                        run = False
+                pygame.draw.rect(self._win, GREEN, button_load_game)
+                self.draw_text_auto_centered(f"{text_button_2}", font_button, BLACK, BUTTON_2_CORDS[1])
+                if click:
+                    self._action = f"{action2}"
+                    run = False
             if button_new_game.collidepoint((x, y)):
                 pygame.draw.rect(self._win, GREEN, button_new_game)
-                self.draw_text("New game", font_button, BLACK, 225, 310)
+                self.draw_text_auto_centered(f"{text_button_1}", font_button, BLACK, BUTTON_1_CORDS[1])
                 if click:
-                    self._action = "New"
+                    self._action = f"{action1}"
                     run = False
             self.update()
             click = False
@@ -104,32 +102,33 @@ class Window:
                 if event.type == MOUSEBUTTONDOWN:
                     if event.button == 1:
                         click = True
+        return self._action
 
     def title_screen(self):
         run = True
-        button_new_game = pygame.Rect(150, 275, 300, 100)
-        button_load_game = pygame.Rect(150, 400, 300, 100)
+        button_new_game = pygame.Rect(BUTTON_1_CORDS[0], BUTTON_1_CORDS[1], BUTTON_WIDTH, BUTTON_HEIGHT)
+        button_load_game = pygame.Rect(BUTTON_2_CORDS[0], BUTTON_2_CORDS[1], BUTTON_WIDTH, BUTTON_HEIGHT)
         click = False
         while run:
             x, y = pygame.mouse.get_pos()
             self._win.fill(WHITE)
             font_title = pygame.font.SysFont(None, FONT_TITLE_HEADER)
             font_button = pygame.font.SysFont(None, FONT_TITLE_BUTONS)
-            self.draw_text("CHAOS AND ORDER", font_title, BLACK, 100, 100)
+            self.draw_text_auto_centered("CHAOS AND ORDER", font_title, BLACK, HEADER_CORDS[1])
             pygame.draw.rect(self._win, GREY, button_new_game)
             pygame.draw.rect(self._win, GREY, button_load_game)
-            self.draw_text("New game", font_button, BLACK, 225, 310)
-            self.draw_text("Load game", font_button, BLACK, 225, 435)
+            self.draw_text_auto_centered("New game", font_button, BLACK, BUTTON_1_CORDS[1])
+            self.draw_text_auto_centered("Load game", font_button, BLACK, BUTTON_2_CORDS[1])
             if button_load_game.collidepoint((x, y)):
                 if self._is_save:
                     pygame.draw.rect(self._win, GREEN, button_load_game)
-                    self.draw_text("Load game", font_button, BLACK, 225, 435)
+                    self.draw_text_auto_centered("Load game", font_button, BLACK, BUTTON_2_CORDS[1])
                     if click:
                         self._action = "Load"
                         run = False
             if button_new_game.collidepoint((x, y)):
                 pygame.draw.rect(self._win, GREEN, button_new_game)
-                self.draw_text("New game", font_button, BLACK, 225, 310)
+                self.draw_text_auto_centered("New game", font_button, BLACK, BUTTON_1_CORDS[1])
                 if click:
                     self._action = "New"
                     run = False
@@ -148,166 +147,18 @@ class Window:
                         click = True
 
     def side_choose_window(self):
-        run = True
-        button_order = pygame.Rect(150, 275, 300, 100)
-        button_chaos = pygame.Rect(150, 400, 300, 100)
-        click = False
-        while run:
-            x, y = pygame.mouse.get_pos()
-            self._win.fill(WHITE)
-            font_title = pygame.font.SysFont(None, FONT_TITLE_HEADER)
-            font_button = pygame.font.SysFont(None, FONT_TITLE_BUTONS)
-            self.draw_text("Choose side", font_title, BLACK, 160, 100)
-            pygame.draw.rect(self._win, GREY, button_order)
-            pygame.draw.rect(self._win, GREY, button_chaos)
-            self.draw_text("Order", font_button, BLACK, 250, 310)
-            self.draw_text("Chaos", font_button, BLACK, 250, 435)
-            if button_chaos.collidepoint((x, y)):
-                pygame.draw.rect(self._win, GREEN, button_chaos)
-                self.draw_text("Chaos", font_button, BLACK, 250, 435)
-                if click:
-                    self._side = "Chaos"
-                    run = False
-            if button_order.collidepoint((x, y)):
-                pygame.draw.rect(self._win, GREEN, button_order)
-                self.draw_text("Order", font_button, BLACK, 250, 310)
-                if click:
-                    self._side = "Order"
-                    run = False
-            self.update()
-            click = False
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    run = False
-                    pygame.quit()
-                if event.type == pygame.KEYDOWN:
-                    if event.key == K_ESCAPE:
-                        run = False
-                        pygame.quit()
-                if event.type == MOUSEBUTTONDOWN:
-                    if event.button == 1:
-                        click = True
+        action = self._generate_basic_window_other_than_title("Choose side", "Order", "Chaos", "Order", "Chaos")
+        self._side = action
 
     def difficulty_choose_window(self):
-        run = True
-        button_easy = pygame.Rect(150, 275, 300, 100)
-        button_hard = pygame.Rect(150, 400, 300, 100)
-        click = False
-        while run:
-            x, y = pygame.mouse.get_pos()
-            self._win.fill(WHITE)
-            font_title = pygame.font.SysFont(None, FONT_TITLE_HEADER)
-            font_button = pygame.font.SysFont(None, FONT_TITLE_BUTONS)
-            self.draw_text("Choose difficulty", font_title, BLACK, 125, 100)
-            pygame.draw.rect(self._win, GREY, button_easy)
-            pygame.draw.rect(self._win, GREY, button_hard)
-            self.draw_text("Easy", font_button, BLACK, 260, 310)
-            self.draw_text("Hard", font_button, BLACK, 260, 435)
-            if button_hard.collidepoint((x, y)):
-                pygame.draw.rect(self._win, GREEN, button_hard)
-                self.draw_text("Hard", font_button, BLACK, 260, 435)
-                if click:
-                    self._gamemode = "Hard"
-                    run = False
-            if button_easy.collidepoint((x, y)):
-                pygame.draw.rect(self._win, GREEN, button_easy)
-                self.draw_text("Easy", font_button, BLACK, 260, 310)
-                if click:
-                    self._gamemode = "Easy"
-                    run = False
-            self.update()
-            click = False
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    run = False
-                    pygame.quit()
-                if event.type == pygame.KEYDOWN:
-                    if event.key == K_ESCAPE:
-                        run = False
-                        pygame.quit()
-                if event.type == MOUSEBUTTONDOWN:
-                    if event.button == 1:
-                        click = True
+        action = self._generate_basic_window_other_than_title("Choose difficulty", "Easy", "Hard", "Easy", "Hard")
+        self._gamemode = action
 
     def game_window_loose(self):
-        run = True
-        button_retry = pygame.Rect(150, 275, 300, 100)
-        button_exit = pygame.Rect(150, 400, 300, 100)
-        click = False
-        while run:
-            x, y = pygame.mouse.get_pos()
-            self._win.fill(WHITE)
-            font_title = pygame.font.SysFont(None, FONT_TITLE_HEADER)
-            font_button = pygame.font.SysFont(None, FONT_TITLE_BUTONS)
-            self.draw_text("You Lose", font_title, BLACK, 205, 100)
-            pygame.draw.rect(self._win, GREY, button_retry)
-            pygame.draw.rect(self._win, GREY, button_exit)
-            self.draw_text("Retry", font_button, BLACK, 260, 310)
-            self.draw_text("Exit", font_button, BLACK, 260, 435)
-            if button_exit.collidepoint((x, y)):
-                pygame.draw.rect(self._win, GREEN, button_exit)
-                self.draw_text("Exit", font_button, BLACK, 260, 435)
-                if click:
-                    self._end_action = "Exit"
-                    run = False
-            if button_retry.collidepoint((x, y)):
-                pygame.draw.rect(self._win, GREEN, button_retry)
-                self.draw_text("Retry", font_button, BLACK, 260, 310)
-                if click:
-                    self._end_action = "Retry"
-                    run = False
-            self.update()
-            click = False
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    run = False
-                    pygame.quit()
-                if event.type == pygame.KEYDOWN:
-                    if event.key == K_ESCAPE:
-                        run = False
-                        pygame.quit()
-                if event.type == MOUSEBUTTONDOWN:
-                    if event.button == 1:
-                        click = True
+        action = self._generate_basic_window_other_than_title("You Lose", "Retry", "Exit", "Retry", "Exit")
+        self._end_action = action
 
 
     def game_window_win(self):
-        run = True
-        button_retry = pygame.Rect(150, 275, 300, 100)
-        button_exit = pygame.Rect(150, 400, 300, 100)
-        click = False
-        while run:
-            x, y = pygame.mouse.get_pos()
-            self._win.fill(WHITE)
-            font_title = pygame.font.SysFont(None, FONT_TITLE_HEADER)
-            font_button = pygame.font.SysFont(None, FONT_TITLE_BUTONS)
-            self.draw_text("You Won", font_title, BLACK, 205, 100)
-            pygame.draw.rect(self._win, GREY, button_retry)
-            pygame.draw.rect(self._win, GREY, button_exit)
-            self.draw_text("Retry", font_button, BLACK, 260, 310)
-            self.draw_text("Exit", font_button, BLACK, 260, 435)
-            if button_exit.collidepoint((x, y)):
-                pygame.draw.rect(self._win, GREEN, button_exit)
-                self.draw_text("Exit", font_button, BLACK, 260, 435)
-                if click:
-                    self._end_action = "Exit"
-                    run = False
-            if button_retry.collidepoint((x, y)):
-                pygame.draw.rect(self._win, GREEN, button_retry)
-                self.draw_text("Retry", font_button, BLACK, 260, 310)
-                if click:
-                    self._end_action = "Retry"
-                    run = False
-            self.update()
-            click = False
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    run = False
-                    pygame.quit()
-                if event.type == pygame.KEYDOWN:
-                    if event.key == K_ESCAPE:
-                        run = False
-                        pygame.quit()
-                if event.type == MOUSEBUTTONDOWN:
-                    if event.button == 1:
-                        click = True
+        action = self._generate_basic_window_other_than_title("You Won", "Retry", "Exit", "Retry", "Exit")
+        self._end_action = action
