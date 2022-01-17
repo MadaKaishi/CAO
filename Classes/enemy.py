5df497701c2a3c6530451224da0259de6601a06c
Adam Misiak <madakaishi@gmail.com>
2022-01-17 11:58:04 +0100

    Improved enemy Order AI

--- Classes/enemy.py
+++ Classes/enemy.py
@@ -1,5 +1,5 @@
+from pickle import TRUE
 from random import choice
-from tkinter.tix import ROW
 from .board import Board
 from .constants import BOARD_SIZE as size, COLS, ROWS
 
@@ -40,6 +40,7 @@ class EnemyAIOrder(Enemy):
     def __init__(self, name="", board=None):
         super().__init__(name)
         self._board = board
+        self._tiers = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0]]
 
     def board(self):
         return self._board
@@ -47,26 +48,105 @@ class EnemyAIOrder(Enemy):
     def choose_index(self, board: "Board"):
         # scan middle 4 x 4 of board
         middle_rect = self._get_middle_rectangle(board)
+        self._middle_rect = middle_rect
         # search for possibilities of placing 4 in row
-        index, action = self._analize_middle_square(middle_rect, "X", "O")
+        collection, index, value = self._analize_middle_square(middle_rect, "X", "O")
         # chooses the best location
-        if action == "row":
-            empty_tiles = []
-            for col in range(1, COLS-1):
-                if middle_rect[index+1, col] == "":
-                    empty_tiles.append((index+1, col))
-            return choice(empty_tiles)
-        if action == "col":
-            empty_tiles = []
-            for row in range(1, ROWS-1):
-                if middle_rect[row, index+1] == "":
-                    empty_tiles.append((row, index+1))
-            return choice(empty_tiles)
-        if action == "diag":
-
+        repeat = True
+        while repeat:
+            repeat = False
+            if collection == 0:
+                if value == 4:
+                    if board.board()[index+1][0].symbol() == "":
+                        chosen = (index+1, 0)
+                    if board.board()[index+1][COLS-1].symbol() == "":
+                        chosen = (index+1, COLS-1)
+                    else:
+                        self._tiers[collection][index] = -1
+                        repeat = TRUE
+                else:
+                    empty_tiles = []
+                    for col in range(COLS-2):
+                        if middle_rect[index][col] == "":
+                            empty_tiles.append((index+1, col+1))
+                    chosen = choice(empty_tiles)
+            if collection == 1:  # if best tier is contained in collumns
+                if value == 4:
+                    if board.board()[0][index+1].symbol() == "":
+                        chosen = (0, index+1)
+                    if board.board()[ROWS-1][index+1].symbol() == "":
+                        chosen = (ROWS-1, index+1)
+                    else:
+                        self._tiers[collection][index] = -1
+                        repeat = True
+                else:
+                    empty_tiles = []
+                    for row in range(ROWS-2):
+                        if middle_rect[row][index] == "":
+                            empty_tiles.append((row+1, index+1))
+                    chosen = choice(empty_tiles)
+            if collection == 2:
+                empty_tiles = []
+                if index == 0:
+                    if value == 4:
+                        if board.board()[0][0].symbol() == "":
+                            chosen = (0, 0)
+                        if board.board()[ROWS-1][COLS-1].symbol() == "":
+                            chosen = (ROWS-1, COLS-1)
+                        else:
+                            self._tiers[collection][index] = -1
+                            repeat = True
+                    else:
+                        for rowcol in range(size-2):
+                            if middle_rect[rowcol][rowcol] == "":
+                                empty_tiles.append((rowcol+1, rowcol+1))
+                        chosen = choice(empty_tiles)
+                if index == 1:
+                    if value == 4:
+                        if board.board()[ROWS-1][0].symbol() == "":
+                            return (ROWS-1, 0)
+                        if board.board()[0][COLS-1].symbol() == "":
+                            chosen = (0, COLS-1)
+                        else:
+                            self._tiers[collection][index] = -1
+                            repeat = True
+                    else:
+                        for rowcol in range(size-2):
+                            if middle_rect[size-3-rowcol][rowcol] == "":
+                                empty_tiles.append((rowcol+1, rowcol+1))
+                        chosen = choice(empty_tiles)
+        self._choose_symbol_atributes = collection, index
+        print(chosen)
+        return chosen
 
     def choose_symbol(self):
-        pass
+        collection, index = self._choose_symbol_atributes
+        if collection == 0:
+            if "X" in self._middle_rect[index]:
+                return "X"
+            else:
+                return "O"
+        if collection == 1:
+            symbols_form_column = []
+            for row in range(size-2):
+                symbols_form_column.append(self._middle_rect[row][index])
+            if "X" in symbols_form_column:
+                return "X"
+            else:
+                return "O"
+        if collection == 2:
+            if index == 0:
+                for rowcol in range(size-2):
+                    if self._middle_rect[rowcol][rowcol] == "X":
+                        return "X"
+                return "O"
+            if index == 1:
+                for rowcol in range(size-2):
+                    if self._middle_rect[size-3-rowcol][rowcol] == "X":
+                        return "X"
+                return "O"
+
+
 
     def _get_middle_rectangle(self, board: "Board"):
         list_board = board.board()
@@ -80,44 +160,31 @@ class EnemyAIOrder(Enemy):
         return middle_rect
 
     def _analize_middle_square(self, middle_square: list, symbol_1, symbol_2):
-        row_best = self._analize_rows(middle_square, symbol_1, symbol_2)
-        col_best = self._analize_cols(middle_square, symbol_1, symbol_2)
-        diag_best = self._analize_diagonals(middle_square, symbol_1, symbol_2)
-        best = max(row_best, col_best, diag_best)
-        if best == row_best:
-            action = "row"
-        if best == col_best:
-            action == "col"
-        if best == diag_best:
-            action = "diag"
-        return best, action
+        self._analize_rows(middle_square, symbol_1, symbol_2)
+        self._analize_cols(middle_square, symbol_1, symbol_2)
+        self._analize_diagonals(middle_square, symbol_1, symbol_2)
+        print(self._tiers)
+        best = 0
+        best_collection = 0
+        best_index = 0
+        for index_1, collection in enumerate(self._tiers):
+            for index_2, tier in enumerate(collection):
+                if tier >= best:
+                    best_collection = index_1
+                    best_index = index_2
+                    best = tier
+        return best_collection, best_index, best
 
     def _analize_rows(self, middle_square: list, symbol_1, symbol_2):
-        row_tiers = {}
-        for row in range(ROWS-2):
-            tier = 0
-            for piece in middle_square[row]:
-                if piece == f"{symbol_1}":
-                    tier += 1
-                if piece == f"{symbol_2}":
-                    tier = 0
-                    break
-            row_tiers[row] = (tier, symbol_1)
-            tier = 0
-            for piece in middle_square[row]:
-                if piece == f"{symbol_2}":
-                    tier += 1
-                if piece == f"{symbol_1}":
-                    tier = 0
-                    break
-            row_tiers[row] = (tier, symbol_2)
-        # checks what row is the best (if tie then first one)
-        for key in row_tiers:
-            actual = 0
-            row = 0
-            if row_tiers[key][0] > actual:
-                row = key
-        return row
+        row_tier_list = self._get_tier_list_from_board(middle_square, symbol_1, symbol_2)
+        row_list = self._tiers[0]
+        new_row_list = []
+        for index in range(len(row_list)):
+            if row_list[index] != -1:
+                new_row_list.append(row_tier_list[index])
+            else:
+                new_row_list.append(-1)
+        self._tiers[0] = new_row_list
 
     def _analize_cols(self, middle_square: list, symbol_1, symbol_2):
         column_list = []
@@ -126,41 +193,52 @@ class EnemyAIOrder(Enemy):
             for row in range(ROWS-2):
                 temp_list.append(middle_square[row][col])
             column_list.append(temp_list)
-        col_best = self._analize_rows(column_list, symbol_1, symbol_2)
-        return col_best
+        col_tier_list = self._get_tier_list_from_board(column_list, symbol_1, symbol_2)
+        col_list = self._tiers[1]
+        new_col_list = []
+        for index in range(len(col_list)):
+            if col_list[index] != -1:
+                new_col_list.append(col_tier_list[index])
+            else:
+                new_col_list.append(-1)
+        self._tiers[1] = new_col_list
 
     def _analize_diagonals(self, middle_square: list, symbol_1, symbol_2):
-        diagonal_tiers = {}
         diagonal_1 = []
         diagonal_2 = []
-        for row in range(ROWS-2):
-            for col in range(COLS-2):
-                diagonal_1.append(middle_square[row][col])
-                diagonal_2.append(middle_square[ROWS-row][col])
+        for index in range(size-2):
+            diagonal_1.append(middle_square[index][index])
+            diagonal_2.append(middle_square[ROWS-3-index][index])
         diagonals = [diagonal_1, diagonal_2]
-        for diagonal in diagonals:
-            tier = 0
-            for piece in diagonal:
-                if piece == f"{symbol_1}":
-                    tier += 1
-                if piece == f"{symbol_2}":
-                    tier = 0
+        diagonal_tier_list = self._get_tier_list_from_board(diagonals, symbol_1, symbol_2)
+        diag_list = self._tiers[2]
+        new_diag_list = []
+        for index in range(len(diag_list)):
+            if diag_list[index] != -1:
+                new_diag_list.append(diagonal_tier_list[index])
+            else:
+                new_diag_list.append(-1)
+        self._tiers[2] = new_diag_list
+
+    def _get_tier_list_from_board(self, symbol_list: list, symbol_1: str, symbol_2: str):
+        tier_list = []
+        for collection in symbol_list:
+            tier_1 = 0
+            tier_2 = 0
+            for piece in collection:
+                if piece == symbol_1:
+                    tier_1 += 1
+                if piece != symbol_1 and piece != "":
+                    tier_1 = -1
                     break
-            diagonal_tiers[row] = (tier, symbol_1)
-            tier = 0
-            for piece in diagonal:
-                if piece == f"{symbol_2}":
-                    tier += 1
-                if piece == f"{symbol_1}":
-                    tier = 0
+            for piece in collection:
+                if piece == symbol_2:
+                    tier_2 += 1
+                if piece != symbol_2 and piece != "":
+                    tier_2 = -1
                     break
-            diagonal_tiers[row] = (tier, symbol_2)
-        for key in diagonal_tiers:
-            actual = 0
-            row = 0
-            if diagonal_tiers[key][0] > actual:
-                row = key
-        return row
+            tier_list.append(max(tier_1, tier_2))
+        return tier_list
 
 
 class EnemyAIChaos(Enemy):
